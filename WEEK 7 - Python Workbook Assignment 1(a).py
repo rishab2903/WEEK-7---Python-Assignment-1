a9dff36647a0531d52324b8036d2f530b4a2819e
rishab2903 <54606079+rishab2903@users.noreply.github.com>
2020-04-02 21:35:45 -0400

    Add files via upload

---
+++ WEEK 7 - Python Workbook Assignment 1(a).py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+df=pd.read_csv('~/Downloads/all_040_in_01.P1.csv')
+df
+
+
+# In[15]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[16]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ WEEK 7 - Python Workbook Assignment 1(b).py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+df=pd.read_excel('~/Downloads/WTN01.xls')
+df
+
+
+# In[ ]:
+
+
+
+
